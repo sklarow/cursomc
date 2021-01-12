57d30e434fefdd386a8c3170206697ba5cc071c1
Allan Matheus Chueiri Sklarow <allan@sklarow.blog.br>
2021-01-12 18:07:13 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/sklarow/cursomc/domain/Categoria.java
+++ src/main/java/com/sklarow/cursomc/domain/Categoria.java
@@ -1,10 +1,9 @@
 package com.sklarow.cursomc.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -16,8 +15,10 @@ public class Categoria implements Serializable {
     private Integer id;
     private String nome;
 
-    public Categoria(){
-    }
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
+
+    public Categoria(){}
 
     public Categoria(Integer id, String nome) {
         this.id = id;
@@ -26,6 +27,14 @@ public class Categoria implements Serializable {
 
     public Integer getId() {
         return id;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     public void setId(Integer id) {
