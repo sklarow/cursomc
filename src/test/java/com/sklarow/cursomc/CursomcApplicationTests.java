f68e301ee5a5e921502fc7fad076c944ead6f56f
Allan Matheus Chueiri Sklarow <allan@sklarow.blog.br>
2021-01-11 23:30:46 -0300

    Projeto criado

---
+++ src/test/java/com/sklarow/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sklarow.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
