240b35cbb4a20f32d6c79bddbcff332b8eb39463
Allan Matheus Chueiri Sklarow <allan@sklarow.blog.br>
2021-01-12 17:40:58 -0300

    Criando repository e service de Categoria

---
+++ src/main/java/com/sklarow/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.sklarow.cursomc.repositories;
+
+import com.sklarow.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
---
+++ src/main/java/com/sklarow/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.sklarow.cursomc.services;
+
+import com.sklarow.cursomc.domain.Categoria;
+import com.sklarow.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
